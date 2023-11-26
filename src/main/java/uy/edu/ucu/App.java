45df3c8711e31eba0a0626c39d4f5b4c9d920d33
RafaelRodriguezSanz <rafaelrodriguezsanz@gmail.com>
2023-11-26 20:00:05 -0300

    Added backend

---
+++ src/main/java/uy/edu/ucu/App.java
@@ -0,0 +1,11 @@
+package uy.edu.ucu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
